cb8c93e0e6cee37b90ac72fcb58b921b01b318ef
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2024-02-04 22:40:35 +0400

    fix

--- src/view/pages/books/Books.js
+++ src/view/pages/books/Books.js
@@ -2,13 +2,12 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import BooksService from "../../../services/BooksService";
 import useAppState from "../../../libs/hooks/useAppState";
-import {actions} from "../../../store/store";
-import {useNavigate} from "react-router-dom";
+import { actions } from "../../../store/store";
+import { useNavigate } from "react-router-dom";
 
 export default function Books({ navigation }) {
   const { state, dispatch } = useAppState();
   const navigate = useNavigate();
-
 
   useEffect(() => {
     function getBooks() {
@@ -29,27 +28,38 @@ export default function Books({ navigation }) {
       });
       dispatch({
         type: actions.SET_BOOKS,
-        payload: books
-      })
-
+        payload: books,
+      });
     }
 
     getBooks();
   }, []);
 
   function handlePress(book) {
-    navigate('/chapter')
+    navigate("/chapter");
   }
 
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', marginTop: 15 }}>
-      <div style={{ maxWidth: 327, textAlign: 'center' }}>
-        {state.home.books.map(b => {
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 15,
+      }}
+    >
+      <div style={{ maxWidth: 327, textAlign: "center" }}>
+        {state.home.books.map((b) => {
           return (
-            <span style={{ padding: '10px 5px'}} onClick={() => handlePress(b)}>{b.abbreviation}</span>
-          )
+            <span
+              style={{ padding: "10px 5px" }}
+              onClick={() => handlePress(b)}
+            >
+              {b.abbreviation}
+            </span>
+          );
         })}
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 }
